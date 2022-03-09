8d1768e80e445b389bcf81664cff1212d4d754d6
KnightriderX <kgurukuntala@gmail.com>
2022-03-09 16:36:37 +0530

    section 6

--- space-finder-backend/infrastructure/SpaceStack.ts
+++ space-finder-backend/infrastructure/SpaceStack.ts
@@ -11,11 +11,20 @@ import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 export class SpaceStack extends Stack {
 
     private api = new RestApi(this, 'SpaceApi');
-    private spacesTable = new GenericTable(
-        'SpacesTable',
-        'spaceId',
-        this
-    )
+    // private spacesTable = new GenericTable(
+    //     'SpacesTable',
+    //     'spaceId',
+    //     this
+    // )
+    private spacesTable = new GenericTable(this, {
+        tableName: 'SpacesTable',
+        primaryKey: 'spaceId',
+        createLambdaPath: 'Create',
+        readLambdaPath: 'Read',
+        updateLambdaPath: 'Update',
+        deleteLambdaPath: 'Delete',
+        secondaryIndexes: ['location']
+    });
 
     constructor(scope: Construct, id: string, props: StackProps) {
         super(scope, id, props)
@@ -41,6 +50,13 @@ export class SpaceStack extends Stack {
         const helloLambdaIntegration = new LambdaIntegration(helloLamdaNodeJs);
         const helloLambdaResource = this.api.root.addResource('hello');
         helloLambdaResource.addMethod('GET', helloLambdaIntegration);
+
+        // Spaces API Integrations:
+        const spaceResource = this.api.root.addResource('spaces');
+        spaceResource.addMethod('POST', this.spacesTable.createLambdaIntegration);
+        spaceResource.addMethod('GET', this.spacesTable.readLambdaIntegration);
+        spaceResource.addMethod('PUT', this.spacesTable.updateLambdaIntegration);
+        spaceResource.addMethod('DELETE', this.spacesTable.deleteLambdaIntegration);
 
     }
 
--- space-finder-backend/test/playground/Hello.test.ts
+++ space-finder-backend/test/playground/Hello.test.ts
@@ -1,3 +1,53 @@
-import { handler } from '../../services/node-lambda/hello';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { handler } from '../../services/SpacesTable/Create';
+// import { handler } from '../../services/SpacesTable/Read';
+// import { handler } from '../../services/SpacesTable/Update';
+// import { handler } from '../../services/SpacesTable/Delete';
 
-handler({}, {});
+const event: APIGatewayProxyEvent = {
+    body: {
+        name: 'someName',
+        location: 'somelocation'
+
+    }
+} as any;
+
+
+
+const result = handler(event, {} as any).then((apiresult) => {
+    const items = JSON.parse(apiresult.body);
+    console.log(123)
+    // console.log(items)
+});
+
+// const event_delete: APIGatewayProxyEvent = {
+//     queryStringParameters: {
+//         spaceId: 'c056b45f-37f4-4b6e-8ac5-4627f573a191'
+//     }
+// } as any;
+// const event_update: APIGatewayProxyEvent = {
+//     queryStringParameters: {
+//         spaceId: 'c056b45f-37f4-4b6e-8ac5-4627f573a191'
+//     },
+//     body: {
+//         location: 'new location'
+//     }
+// } as any;
+
+// const event_old = {
+//     body: {
+//         location: 'Paris'
+//     }
+// }
+
+// const event: APIGatewayProxyEvent = {
+//     queryStringParameters: {
+//         spaceId: 'ad099c86-e762-424b-8eba-ebe5c732cbd6'
+//     }
+// } as any;
+
+// const eventLocation: APIGatewayProxyEvent = {
+//     queryStringParameters: {
+//         location: 'London'
+//     }
+// } as any;
